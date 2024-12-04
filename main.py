0392fc17682108848bd73d1ded5ac8c57f007304
clayvin04 <clayvin04@gmail.com>
2024-12-04 16:01:41 +0100

    first coomit

---
+++ main.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+
+class Base (DataRequired):
+    pass  
+    
+db = SQLAlchemy(model_class=Base)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+db.init_app(app)
+# CREATE DB
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+# CREATE TABLE
+with app.app_context():
+    db.create_all()
+
+    if not Movie.query.filter_by(title="Phone Booth").first():
+        new_movie = Movie(
+            title="Phone Booth",
+            year=2002,
+            description="Publicist Stuart Shepard finds himself trapped in a phone booth...",
+            rating=7.3,
+            ranking=10,
+            review="My favourite character was the caller.",
+            img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+        )
+
+        second_movie = Movie(
+            title="Avatar The Way of Water",
+            year=2022,
+            description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+            rating=7.3,
+            ranking=9,
+            review="I liked the water.",
+            img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+        )
+        # Add the new movie to the session and commit changes
+        # 2) Add the movie 🎞️ to the shelf in the library 🗄️:
+        db.session.add(new_movie)
+        db.session.add(second_movie)
+
+        # 3) Commit the place of the new movie 🎞️:
+        db.session.commit()
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+
+
+    
+@app.route("/")
+def home():
+    result = db.session.execute(db.select(Movie))
+    all_movies = result.scalars().all()
+    return render_template("index.html", movies=all_movies)
+
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", movie=movie, form=form)
+
+app.route("/delete")
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
