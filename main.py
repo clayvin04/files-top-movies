1ec35155f4d471e5bff164ea8a39b836c04dcb07
clayvin04 <clayvin04@gmail.com>
2024-12-04 17:44:28 +0100

    2commit

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from wtforms.validators import DataRequired
 import requests
 
 
+MOVIE_DB_API_KEY = "NOT_A_REAL_KEY"
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
@@ -48,7 +54,7 @@ with app.app_context():
         )
 
         second_movie = Movie(
-            title="Avatar The Way of Water",
+            title="Avatar The Way of Wate",
             year=2022,
             description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
             rating=7.3,
@@ -68,6 +74,10 @@ class RateMovieForm(FlaskForm):
     rating = StringField("Your Rating Out of 10 e.g. 7.5")
     review = StringField("Your Review")
     submit = SubmitField("Done")
+    
+class FindMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
 
 
     
@@ -76,6 +86,38 @@ def home():
     result = db.session.execute(db.select(Movie))
     all_movies = result.scalars().all()
     return render_template("index.html", movies=all_movies)
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    form = FindMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
+
+        data = response.json()["results"]
+        
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+      
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        
+        # Redirect to /edit route
+        return redirect(url_for("rate_movie", id=new_movie.id))
+
 
 @app.route("/edit", methods=["GET", "POST"])
 def rate_movie():
@@ -89,13 +131,17 @@ def rate_movie():
         return redirect(url_for('home'))
     return render_template("edit.html", movie=movie, form=form)
 
-app.route("/delete")
-def delete_movie():
+@app.route("/delete", methods=["GET", "POST"])
+def delete():
     movie_id = request.args.get("id")
     movie = db.get_or_404(Movie, movie_id)
     db.session.delete(movie)
     db.session.commit()
     return redirect(url_for("home"))
+
+
+      
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
